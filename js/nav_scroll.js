53fca6a4877d4463d57100358b2ef5d829e23f9d
Aziz Ramos <sonicthehamilton.ar@gmail.com>
2017-11-26 11:44:49 -0500

    add video hero

--- js/nav_scroll.js
+++ js/nav_scroll.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
 	$(window).scroll(function() {
-		var checkpoint = $('#hero').height();
+		var checkpoint = $('#video_hero').height();
 		if ($('#bldy-nav').offset().top >= checkpoint ) {
 			$('.cls-1').css('fill', 'rgb(0, 0, 0)');
 			$('.share_area ul li p').css("color", "black");
 			$('.share_area ul li a .fa.fa-twitter').css("color", "black");
 			$('.share_area ul li a .fa.fa-facebook').css("color", "black");
+			$('.share_area ul li a .social-counter').css("color", "black");
 		} else {
 			$('.cls-1').css('fill', 'rgb(255, 255, 255)');
 			$('.share_area ul li p').css("color", "white");
 			$('.share_area ul li a .fa.fa-twitter').css("color", "white");
 			$('.share_area ul li a .fa.fa-facebook').css("color", "white");
+			$('.share_area ul li a .social-counter').css("color", "white");
 		}
 	});
 });
